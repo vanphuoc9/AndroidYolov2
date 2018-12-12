8a0aa26c2f8c537b1bfa352b6c22ba8d8519d255
vanphuoc <thaiphuoc1997@gmail.com>
2018-12-12 08:31:30 +0700

    first

--- src/org/tensorflow/yolo/YOLOClassifier.java
+++ src/org/tensorflow/yolo/YOLOClassifier.java
@@ -24,10 +24,14 @@ import java.util.Vector;
 public class YOLOClassifier {
     private final static float OVERLAP_THRESHOLD = 0.5f;
     private final static double anchors[] = {1.08,1.19,  3.42,4.41,  6.63,11.38,  9.42,5.11,  16.62,10.52};
+    //private final static int SIZE = 13;
     private final static int SIZE = 13;
-    private final static int MAX_RECOGNIZED_CLASSES = 13;
+    //private final static int MAX_RECOGNIZED_CLASSES = 13;
+    private final static int MAX_RECOGNIZED_CLASSES = 1;
     private final static float THRESHOLD = 0.3f;
-    private final static int MAX_RESULTS = 15;
+    //private final static int MAX_RESULTS = 15;
+    private final static int MAX_RESULTS = 1;
+
     private final static int NUMBER_OF_BOUNDING_BOX = 5;
     private static YOLOClassifier classifier;
 
